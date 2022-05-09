7c6273e736df53f20cd1b1ab7aec9ed1602a2ce1
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-05-09 18:46:20 -0400

    Project created

---
+++ src/main/java/com/uasp/hhrr/SistemaDeGestionDeRecursosHumanosApplication.java
@@ -0,0 +1,13 @@
+package com.uasp.hhrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeGestionDeRecursosHumanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeGestionDeRecursosHumanosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uasp/hhrr/SistemaDeGestionDeRecursosHumanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uasp.hhrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeGestionDeRecursosHumanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
