e0a3462ca8bc096d18115dfa95c07aab8e225ec5
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-05-19 18:00:42 -0400

    CORS Origins Corrected

--- src/main/java/com/uasp/hhrr/security/config/WebSecurityConfig.java
+++ src/main/java/com/uasp/hhrr/security/config/WebSecurityConfig.java
@@ -89,7 +89,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+//        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
