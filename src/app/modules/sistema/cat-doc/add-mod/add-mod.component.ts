869736261d8ebd8ba182e0dd2b181fcd2e30b6b7
Daniel González Cuétara <danny.glezcuet98@gmail.com>
2022-05-27 23:02:48 -0400

    Cat-Doc

--- src/app/modules/sistema/cat-doc/add-mod/add-mod.component.ts
+++ src/app/modules/sistema/cat-doc/add-mod/add-mod.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit, Optional } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { CatDoc } from 'src/app/data/schema';
 
@@ -26,15 +26,15 @@ export class CDocAddModComponent {
     }
 
     this.form = new FormGroup({
-      // clasificador: new FormControl({ value: this.object.clasificador, disabled: this.mydata.isMod }, Validators.required),
-      // salario: new FormControl(this.object.salario, [Validators.min(0), Validators.required])
+      tipo: new FormControl({ value: this.object.tipo, disabled: this.mydata.isMod }, Validators.required),
+      salario: new FormControl(this.object.salario, [Validators.min(0), Validators.required])
     })
   }
 
   closeDialog() {
     if (this.form.valid) {
-      // this.object.clasificador = this.form.get('clasificador')?.value
-      // this.object.salario = this.form.get('salario')?.value
+      this.object.tipo = this.form.get('tipo')?.value
+      this.object.salario = this.form.get('salario')?.value
 
       this.dialogRef.close({ success: true, object: this.object });
     }
--- src/app/modules/sistema/cat-doc/cat-doc.component.ts
+++ src/app/modules/sistema/cat-doc/cat-doc.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { CatDoc, ServicesConsumer } from 'src/app/data/schema';
+import { CatDocService } from 'src/app/data/services';
+import { DelDialogComponent } from 'src/app/shared/components';
+import { CDocAddModComponent } from './add-mod/add-mod.component';
 
 @Component({
   selector: 'app-cat-doc',
   templateUrl: './cat-doc.component.html',
   styleUrls: ['./cat-doc.component.css']
 })
-export class CatDocComponent implements OnInit {
+export class CatDocComponent extends ServicesConsumer<CatDoc, number>{
 
-  constructor() { }
+  displayedColumns: string[] = ['tipo', 'salario', 'actions'];
+  dataSource = new MatTableDataSource<CatDoc>([]);
+  faAdd = faPlus; faEdit = faPencil; faDelete = faTrash
 
-  ngOnInit(): void {
+  @ViewChild(MatPaginator) set matPaginator(paginator: MatPaginator) {
+    this.dataSource.paginator = paginator
+  }
+
+  @ViewChild(MatSort) set matSort(sort: MatSort) {
+    this.dataSource.sort = sort
+  }
+
+  constructor(
+    service: CatDocService,
+    router: Router,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
+  ) {
+    super(service, router)
+  }
+
+  override refreshData() {
+    this.service.getAll().subscribe(
+      r => {
+        if (!r.error) {
+          this.data = r.data;
+          this.dataSource = new MatTableDataSource(this.data);
+
+          console.log(this.dataSource + '\n' + r.status)
+          setTimeout(() => this.isLoading = false, 1000)
+        } else {
+          this.router.navigateByUrl('/home');
+        }
+      }
+    )
+  }
+
+  override sendMsg(msg: string) {
+    this.snackBar.open(msg, '', { duration: 2000, horizontalPosition: 'end' })
+  }
+
+  applyFilter(event: Event) {
+    let filterValue = (event.target as HTMLInputElement).value
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  addModDialog(isMod: boolean, object?: CatDoc) {
+    const myCompDialog = this.dialog.open(CDocAddModComponent, { data: { isMod: isMod, object: object ? object : undefined } });
+    myCompDialog.afterClosed().subscribe((res) => {
+      if (res)
+        if (res.success) {
+          if (!isMod) {
+            this.add(res.object)
+            this.sendMsg('Categoría agregada correctamente')
+          } else {
+            this.mod(res.object, res.object.id)
+            this.sendMsg('Categoría modificada correctamente')
+          }
+        }
+    });
+  }
+
+  delDialog(object: CatDoc) {
+    const myCompDialog = this.dialog.open(DelDialogComponent, { data: { text: 'la categoría docente ', class: object.tipo } });
+    myCompDialog.afterClosed().subscribe((res) => {
+      if (res.event == 'yes-option') {
+        this.del(object.id)
+      }
+    });
   }
 
 }
